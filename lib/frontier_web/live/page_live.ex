1dae7a7b7cd2a98f2817733932431a6e4d99f1bc
alisha-develops <alishalish107@gmail.com>
2026-08-24 19:28:06 +0500

    feat: add interactive puzzle and reveal animation

--- lib/frontier_web/live/page_live.ex
+++ lib/frontier_web/live/page_live.ex
@@ -9,23 +9,31 @@ defmodule FrontierWeb.PageLive do
   def mount(_params, _session, socket) do
     if connected?(socket) do
       Phoenix.PubSub.subscribe(Frontier.PubSub, @topic)
-
       Presence.track(self(), @topic, socket.id, %{})
     end
 
     viewer_count = map_size(Presence.list(@topic))
 
-    socket = assign(socket, viewer_count: viewer_count)
-
-    {:ok, socket}
+    {:ok,
+     assign(socket,
+       viewer_count: viewer_count,
+       unlocked: false
+     )}
   end
 
   @impl true
   def handle_info(%{event: "presence_diff"}, socket) do
     viewer_count = map_size(Presence.list(@topic))
 
-    socket = assign(socket, viewer_count: viewer_count)
+    {:noreply, assign(socket, :viewer_count, viewer_count)}
+  end
 
-    {:noreply, socket}
+  @impl true
+  def handle_event("decode", %{"answer" => answer}, socket) do
+    if String.downcase(String.trim(answer)) == "frontier" do
+      {:noreply, assign(socket, :unlocked, true)}
+    else
+      {:noreply, socket}
+    end
   end
 end
